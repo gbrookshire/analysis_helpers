7b26592ac31b6d46a0de99ee30645beb0d3aa455
G Brookshire <broog731@newschool.edu>
2016-12-23 17:39:01 -0800

    Bugfix: fix.factors
    Fixed bug that prevented it from working with factors with only 1 level.

--- scripts/FixFactors.R
+++ scripts/FixFactors.R
@@ -6,16 +6,19 @@ fix.factors <- function(dat) {
 	# Takes a data.frame and returns a new version of it
 	
 	for (field in names(dat)) {
-    
     	# get rid of unused factor levels
-	    if (is.factor(dat[,field]))
+	    if (is.factor(dat[,field])) {
+	      # Remove unused levels
 	      dat[,field] <- factor(dat[,field][])
-	
-	    # move to deviation coding (mean-centered)
-	    contrasts(dat[,field]) <- deviation.code(dat[,field],
-	                                             levels(dat[,field])[1])
-	    # The baseline level doesn't affect the output of summary(aov(...)),
-	    # so this script randomly chooses a baseline level of the factor.
+	      if (nlevels(dat[,field]) > 1) {
+  	      # move to deviation coding (mean-centered)
+  	      contrasts(dat[,field]) <- deviation.code(dat[,field],
+  	                                               levels(dat[,field])[1])
+  	      # The baseline level doesn't affect the output of summary(aov(...)),
+  	      # so this script randomly chooses a baseline level of the factor.
+	      }
+	    }
+	      
 	}
 	return(dat)
 }
